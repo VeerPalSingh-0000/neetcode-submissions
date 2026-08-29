4b227c0895c6af3d172331f5d9c58cfa61d4b63c
Veer Pal Singh <sveerpal7727@gmail.com>
2026-08-29 12:43:32 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> f1(26,0);
+        vector<int> f2(26,0);
+
+        if(s.size() != t.size()) return false;
+
+        for(int i=0;i<s.size();i++){
+            f1[s[i]-'a']++;
+            f2[t[i]-'a']++;
+        }
+
+        return f1 == f2;
+    }
+};
