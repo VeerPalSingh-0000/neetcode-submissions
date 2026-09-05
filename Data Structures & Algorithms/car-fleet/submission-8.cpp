89c29d3f08a0580f17a4d2a804eb075a235365ee
Veer Pal Singh <sveerpal7727@gmail.com>
2026-09-05 11:31:22 +0530

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        unordered_map<int,double> m;
+       
+
+        for(int i=0;i<speed.size();i++){
+            double x = double(target-position[i])/speed[i];
+            
+                m[position[i]] = x;
+            
+        }
+
+        sort(position.begin(), position.end());
+
+        stack<int> s;
+
+        for(int i=0;i<position.size();i++){
+            s.push(position[i]);
+        }
+
+        int count = 0;
+
+        while(!s.empty()){
+
+            double maxi = m[s.top()]; s.pop();
+            while(!s.empty() && m[s.top()] <= maxi){
+                s.pop();
+            }
+            count++;
+        }
+
+    return count;
+        
+    }
+};
