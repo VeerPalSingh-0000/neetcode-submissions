a9876ca4a8ac519663cd7a463ad9bc1545732c4d
Veer Pal Singh <sveerpal7727@gmail.com>
2026-09-07 12:10:46 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                ListNode* newNode = new ListNode(list1->val);
+                list1 = list1->next;
+                if(head == NULL){
+                head = tail = newNode;
+            }else{
+                tail->next = newNode;
+                tail = newNode;
+                }
+            }else{
+                ListNode* newNode = new ListNode(list2->val);
+                list2 = list2->next;
+                if(head == NULL){
+                head = tail = newNode;
+                }else{
+                tail->next = newNode;
+                tail = newNode;
+                }
+            }
+
+        }
+
+        while(list1 != NULL){
+            ListNode* newNode = new ListNode(list1->val);
+            list1 = list1->next;
+            if(head == NULL){
+                head = tail = newNode;
+            }else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        while(list2 != NULL){
+            ListNode* newNode = new ListNode(list2->val);
+            list2 = list2->next;
+            if(head == NULL){
+                head = tail = newNode;
+            }else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+};
