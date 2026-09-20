fb67ac74b80af2fb22fd7022c78a6222759dc257
Veer Pal Singh <sveerpal7727@gmail.com>
2026-09-20 11:14:57 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void helper(vector<int>& nums, int idx, int target, vector<int>& temp, vector<vector<int>>& ans){
+        if(target < 0 || idx == nums.size()) return;
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[idx]);
+        helper(nums, idx+1, target-nums[idx], temp, ans);
+
+        temp.pop_back();
+        int new_idx = idx+1;
+        while(new_idx < nums.size() && nums[new_idx] == nums[idx]) new_idx++;
+        helper(nums, new_idx, target, temp, ans);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        helper(nums, 0, target, temp, ans);
+
+        return ans;
+    }
+};
